009d09381aea11ddc943af395fc1a1a3ed4bb58a
Oleksiy Blavat <blavat.alex@gmail.com>
2026-01-10 17:28:02 -0500

    adding CRUD routes for products

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,24 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  get "/products", to: "products#index"
+
+  # create a new product
+  post "/products", to: "products#create"
+  get "/products/new", to "products#create"
+  
+  # get a product by id or name
+  get "/products/:id", to: "products#show"
+  get "/products/:name", to: "products#show"
+  
+  # update a product
+  get "/products/:id/edit", to: "products#edit"
+  patch "/products/:id", to: "products#update"
+  put "/products/:id", to: "products#update"
+  
+  # delete a product
+  delete "/products/:id", to: "products#destroy"
+
+  # to do all of the above in one line, you can use the following:
+  # resources :products
 end
