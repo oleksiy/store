0442a433def311b092060028101beba7293f6ca4
Oleksiy Blavat <blavat.alex@gmail.com>
2026-01-10 17:36:58 -0500

    Alias all routes for context - they will come up in bin/rails routes when listed

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/products", to: "products#index"
+  get "/products", to: "products#index" as: "list all products via GET"
 
   # create a new product
-  post "/products", to: "products#create"
-  get "/products/new", to: "products#create"
+  post "/products", to: "products#create" as: "create a product via POST"
+  get "/products/new", to: "products#create" as: "create a new product"
   
   # get a product by id or name
-  get "/products/:id", to: "products#show"
-  get "/products/:name", to: "products#show"
+  get "/products/:id", to: "products#show" as: "retrieve by id"
+  get "/products/:name", to: "products#show" as: "retrieve by name"
   
   # update a product
-  get "/products/:id/edit", to: "products#edit"
-  patch "/products/:id", to: "products#update"
-  put "/products/:id", to: "products#update"
+  get "/products/:id/edit", to: "products#edit" as: "edit a product by id"
+  patch "/products/:id", to: "products#update" as: "update a product by id"
+  put "/products/:id", to: "products#update" as: "update a product by id"
   
   # delete a product
-  delete "/products/:id", to: "products#destroy"
+  delete "/products/:id", to: "products#destroy" as: "delete a product by id"
 
   # to do all of the above in one line, you can use the following:
   # resources :products
