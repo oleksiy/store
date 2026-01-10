6466d06d85de158f3167f7db9b024cc16bb90d69
Oleksiy Blavat <blavat.alex@gmail.com>
2026-01-10 17:46:06 -0500

    routes aliases fixed - can't have spaces or be named the same

--- config/routes.rb
+++ config/routes.rb
@@ -3,23 +3,23 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get "/products", to: "products#index", as: "list all products via GET"
+  get "/products", to: "products#index", as: "products"
 
   # create a new product
-  post "/products", to: "products#create", as: "create a product via POST"
-  get "/products/new", to: "products#create", as: "create a new product"
+  post "/products", to: "products#create", as: "products_create"
+  get "/products/new", to: "products#create", as: "products_new"
   
   # get a product by id or name
-  get "/products/:id", to: "products#show", as: "retrieve by id"
-  get "/products/:name", to: "products#show", as: "retrieve by name"
+  get "/products/:id", to: "products#show", as: "products_show"
+  get "/products/:name", to: "products#show", as: "products_show_by_name"
   
   # update a product
-  get "/products/:id/edit", to: "products#edit", as: "edit a product by id"
-  patch "/products/:id", to: "products#update", as: "update a product by id"
-  put "/products/:id", to: "products#update", as: "update a product by id"
+  get "/products/:id/edit", to: "products#edit", as: "products_edit_by_id"
+  patch "/products/:id", to: "products#update", as: "products_update_by_id_patch"
+  put "/products/:id", to: "products#update", as: "products_update_by_id_put"
   
   # delete a product
-  delete "/products/:id", to: "products#destroy", as: "delete a product by id"
+  delete "/products/:id", to: "products#destroy", as: "products_destroy"
 
   # to do all of the above in one line, you can use the following:
   # resources :products
