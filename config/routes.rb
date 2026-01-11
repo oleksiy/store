4b69a7712f1bd58beb54c8f3b5063702a71268cf
Oleksiy Blavat <blavat.alex@gmail.com>
2026-01-11 13:22:51 -0500

    :computer: refactoring to use the alias from the route to access individual product

--- config/routes.rb
+++ config/routes.rb
@@ -4,16 +4,16 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   # root "articles#index"
   root "products#index"
-  
+
   get "/products", to: "products#index", as: "products"
 
   # create a new product
   post "/products", to: "products#create", as: "products_create"
   get "/products/new", to: "products#create", as: "products_new"
   
-  # get a product by id or name
-  get "/products/:id", to: "products#show", as: "products_show"
-  get "/products/:name", to: "products#show", as: "products_show_by_name"
+  # get a product by id or name - when you refer to these in erb files, you have to match the alias you used in the as:
+  get "/products/:id", to: "products#show", as: "product_show"
+  get "/products/:name", to: "products#show", as: "product_show_by_name"
   
   # update a product
   get "/products/:id/edit", to: "products#edit", as: "products_edit_by_id"
