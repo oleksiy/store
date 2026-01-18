fa901ae814f48e8486fc07c0a7a9d1efc9d3947a
Oleksiy Blavat <blavat.alex@gmail.com>
2026-01-18 17:42:00 -0500

    fixed typo in migration

--- db/migrate/20260118223858_create_subscribers.rb
+++ db/migrate/20260118223858_create_subscribers.rb
@@ -1,7 +1,7 @@
 class CreateSubscribers < ActiveRecord::Migration[8.1]
   def change
     create_table :subscribers do |t|
-      t.belongs_to :prodcut, null: false, foreign_key: true
+      t.belongs_to :product, null: false, foreign_key: true
       t.string :email
 
       t.timestamps
