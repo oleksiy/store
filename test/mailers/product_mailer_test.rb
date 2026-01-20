2a4d8f80ebc9c3eadc96a40baed57d6339f2bfec
Oleksiy Blavat <blavat.alex@gmail.com>
2026-01-19 19:55:30 -0500

    added via agent missing test data and fixtures/fixed NPEs

--- test/mailers/product_mailer_test.rb
+++ test/mailers/product_mailer_test.rb
@@ -2,10 +2,13 @@ require "test_helper"
 
 class ProductMailerTest < ActionMailer::TestCase
   test "in_stock" do
-    mail = ProductMailer.in_stock
+    product = products(:one)
+    subscriber = subscribers(:one)
+
+    mail = ProductMailer.with(product: product, subscriber: subscriber).in_stock
     assert_equal "In stock", mail.subject
     assert_equal [ "to@example.org" ], mail.to
     assert_equal [ "from@example.com" ], mail.from
-    assert_match "Hi", mail.body.encoded
+    assert_match "Good news!", mail.body.encoded
   end
 end
