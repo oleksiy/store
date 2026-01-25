2a8b7dad97ecfe68dca4e8caf11d0ae244e8f27a
Oleksiy Blavat <blavat.alex@gmail.com>
2026-01-25 18:07:36 -0500

    added test cases for new fixtures fro subscribers and products

--- test/mailers/product_mailer_test.rb
+++ test/mailers/product_mailer_test.rb
@@ -8,7 +8,15 @@ class ProductMailerTest < ActionMailer::TestCase
     mail = ProductMailer.with(product: product, subscriber: subscriber).in_stock
     assert_equal "In stock", mail.subject
     assert_equal [ "to@example.org" ], mail.to
-    assert_equal [ "from@example.com" ], mail.from
+    assert_equal [ "from@example.org" ], mail.from
+    assert_match "Good news!", mail.body.encoded
+  end
+
+  test "in_stock 2" do
+    mail = ProductMailer.with(product: products(:tshirt), subscriber: subscribers(:david)).in_stock
+    assert_equal "In stock", mail.subject
+    assert_equal ["david@example.org"], mail.to
+    assert_equal ["from@example.org"], mail.from
     assert_match "Good news!", mail.body.encoded
   end
 end
--- test/models/product_test.rb
+++ test/models/product_test.rb
@@ -1,7 +1,18 @@
 require "test_helper"
 
 class ProductTest < ActiveSupport::TestCase
+  include ActionMailer::TestHelper
   # test "the truth" do
   #   assert true
   # end
+  test "send email notis when back in stock" do
+    product = products(:tshirt)
+
+    # Set product to out of stock
+    product.update(inventory_count: 0)
+
+    assert_emails 2 do
+      product.update(inventory_count: 99)
+    end
+  end
 end
